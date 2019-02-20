720b76a815c84e80070f1f9f609f0d786a121f89
isx53320159 <isx53320159@i12.informatica.escoladeltreball.org>
2019-02-20 11:56:17 +0100

    clase

---
+++ k18:khost/install.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+cp /opt/docker/krb5.conf /etc/krb5.conf
---
+++ k18:khost/startup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# @edt ASIX M06 2018-2019
+# startup.sh
+# -------------------------------------
+
+/opt/docker/install.sh && echo "Dades instalades"
+/bin/bash
+
+
